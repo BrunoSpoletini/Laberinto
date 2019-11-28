0c97f3d0277621ad97b2efea1d972db499e9cf75
unknown <bruno_spoletini@hotmail.com>
2019-11-28 19:43:39 -0300

    Tests incompletos

--- ResolvedorDeLaverintos.py
+++ ResolvedorDeLaverintos.py
@@ -7,13 +7,15 @@ def crearLaberinto():
         print("La creacion del laberinto fallo, el programa va a cerrarese")
         sys.exit()
 
-#Hacer pytest
+
 def leerArchivo(archivo):
     f = open (archivo,'r')
     matriz = f.read()
     f.close()
+    matriz=matriz.split()
     return matriz
-#comentar y hacer testeo
+
+
 def obtenerInicio(matriz):
     dimension=len(matriz[0])
     inicio=[]
@@ -30,68 +32,83 @@ def verificar(matriz, x, y , visitados):
         return False
     return not(matriz[y][x]=='1' or ([x,y] in visitados))
 
+#Esta funcion siempre devuelve true, y esta hecha para acotar el codigo repetido
+# pero a la vez lo hace mas confuso, la dejo o la saco?
+def visitado(visitados, queue, x, y):
+    visitados.append([x,y])
+    queue.append([x,y])
+    
 def resolverLaberinto(matriz):
     inicio=obtenerInicio(matriz)
-    flag=True
+
     encontrado=False
     pos=inicio
     camino=[]
     visitados=[inicio]
     queue=[inicio] 
     while queue:
-        if(matriz[pos[1]][pos[0]]=='X'):
-            encontrado=True
-            camino=queue.copy()
-            camino.append(pos)
-        pos=queue.pop()
-        flag=True
+        pos=queue[len(queue)-1]
         if(verificar(matriz,pos[0],pos[1]-1,visitados)):
-            flag=False
-            visitados.append([pos[0],pos[1]-1])
-            queue.append(pos)
-            queue.append([pos[0],pos[1]-1])
-
-        if(flag and verificar(matriz, pos[0]+1,pos[1],visitados)):
-            flag=False
-            visitados.append([pos[0]+1,pos[1]])
-            queue.append(pos)
-            queue.append([pos[0]+1,pos[1]])
-
-        if(flag and verificar(matriz, pos[0],pos[1]+1,visitados)):
-            flag=False
-            visitados.append([pos[0],pos[1]+1])
-            queue.append(pos)
-            queue.append([pos[0],pos[1]+1])
-        
-        if(flag and verificar(matriz, pos[0]-1,pos[1],visitados)):
-            flag=False
-            visitados.append([pos[0]-1,pos[1]])
-            queue.append(pos)
-            queue.append([pos[0]-1,pos[1]])  
+            visitado(visitados, queue, pos[0], pos[1]-1)
+        elif(verificar(matriz, pos[0]+1,pos[1],visitados)):
+            visitado(visitados, queue, pos[0]+1, pos[1])
+        elif(verificar(matriz, pos[0],pos[1]+1,visitados)):
+            visitado(visitados, queue, pos[0], pos[1]+1)
+        elif(verificar(matriz, pos[0]-1,pos[1],visitados)):
+            visitado(visitados, queue, pos[0]-1, pos[1]) 
+        else:
+            if(matriz[pos[1]][pos[0]]=='X'):
+                encontrado=True
+                camino=queue.copy()
+            queue.pop()
     if(encontrado):
         return camino
     else:
         return [[-1,-1]]
-    
+
+def generarCamino(matriz,archivo):
+    camino=resolverLaberinto(matriz)
+    while (camino==[[-1,-1]]):
+        crearLaberinto()
+        matriz=leerArchivo(archivo)
+        camino=resolverLaberinto(matriz)
+    return camino
+
+def imprimirSalida(camino):
+    f = open ('salida.txt','w+')
+    list=[]
+    for pos in camino:
+        list.append("(" + str(pos[0]+1) + "," + str(pos[1]+1) + ")")
+    list=''.join(list)
+    f.write(list)
+    f.close()
+
+
+def testLeerArchivo(laberinto):
+    matriz=leerArchivo(laberinto)
+    assert(matriz == ['I0111', '10001', '00100', 'X0000', '00000'] )
+
+def testObtenerInicio(laberinto):
+    f=open(laberinto)
+    inicio=obtenerInicio(laberinto)
+    f.close()
+    assert inicio,[0, 0]
+
+def tests(laberinto):
+    testLeerArchivo(laberinto)
+    #testObtenerInicio(laberinto)
 
 def main():
     #esto esta hardcodeado, cambiar para pasarlo como param al main
     archivo="laberinto.txt"
     #########
-    camino=[]
+
     crearLaberinto()
-    lista=leerArchivo(archivo)
-    matriz=lista.split()
-    camino=resolverLaberinto(matriz)
-    
-    while (camino==[[-1,-1]]):
-        crearLaberinto()
-        lista=leerArchivo(archivo)
-        matriz=lista.split()
-        camino=resolverLaberinto(matriz)
-    for pos in camino:
-        pos[0]+=1
-        pos[1]+=1
-    print("camino=",camino)
+    matriz=leerArchivo(archivo)
+    camino=generarCamino(matriz,archivo)
+    imprimirSalida(camino)
+    tests("./testPython/laberinto1.txt")
+
 
 main()
+
